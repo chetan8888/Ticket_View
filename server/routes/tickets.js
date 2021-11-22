b9cad1460ccbf3da40741897abd9398d5a3ebacd
CHETAN GOUR <chetangour88@gmail.com>
2021-11-21 23:00:03 -0800

    list tickets api added, concerns remaining

--- server/routes/tickets.js
+++ server/routes/tickets.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const logger = require('../utils/Logger');
-
+var axios = require('axios');
 const router = express.Router();
 
 // List of tickets
 router.get("/list_tickets", (req, res) => 
 {
-    // var zendesk = require('node-zendesk');
-
-    // var client = zendesk.createClient({
-    // username:  'chetangour88@gmail.com',
-    // token:     'rwhR69mzFSaumgU0ToUVWMqKCHYs9O1vVAKGlD8s',
-    // remoteUri: 'https://zccchetangour.zendesk.com/api/v2/tickets.json'
-    // });
-
-    // client.users.list(function (err, req, result) {
-    // if (err) {
-    //     console.log(err);
-    //     return;
-    // }
-    // res.send(result);
-    // });
+    var ticket_list_api = 
+    {
+        method: 'get',
+        url: 'https://' + process.env.mysubdomain + '.zendesk.com/api/v2/tickets.json',
+        headers: 
+        { 
+          'Authorization': 'Basic Y2hldGFuZ291cjg4QGdtYWlsLmNvbTpHb2V4dHJlbWVAMQ==', 
+          'Cookie': '__cfruid=788e870bd1bc20a5da3d23592017b61a1c39e470-1637561892'
+        }
+    };
+      
+    axios(ticket_list_api)
+    .then(function (response)
+    {
+        res.send(response.data)
+    })
+    .catch(function (error)
+    {
+        res.send(error)
+    });
 });
 
 module.exports = router;
